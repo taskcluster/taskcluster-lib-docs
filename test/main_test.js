6411c5c4d2ebd32c51c6dbedf1e7432ebac9236b
Kristel Teng <kristelteng@gmail.com>
2016-07-05 13:44:43 -0400

    test tarball contains schema

--- test/main_test.js
+++ test/main_test.js
@@ -4,6 +4,8 @@ suite('End to End', () => {
   let documenter = require('../');
   let debug = require('debug')('test');
   let validator = require('taskcluster-lib-validate');
+  let _ = require('lodash');
+  let tar = require('tar-stream');
 
   let validate = null;
 
@@ -11,22 +13,63 @@ suite('End to End', () => {
     validate = await validator({
       folder: path.join(__dirname, 'schemas'),
       baseUrl: 'http://localhost:1203/',
-      constants: {},
+      constants: {'my-constant': 42},
     });
   });
 
   test('tarball exists', async function() {
     let schemas = [
-      {name: 'foo.json', content: 'foo'},
-      {name: 'bar.json', content: 'bar'},
+      {id: 'http://example.com/foo.schema.json', schema: 'http://json-schema.org/draft-04/schema#'},
+      {id: 'http://example.com/bar.schema.json', schema: 'http://json-schema.org/draft-04/schema#'},
     ];
     let doc = await documenter({
-      //Schema name + content
-      schemas,
+      schemas, // schema.id + content
+    });
+    assert.ok(doc.tarball); // testing tarball exists
+  });
+
+  test('tarball contains schemas with prefix', async function(done) {
+    let schemas = [
+      {id: 'http://example.com/foo.schema.json', schema: 'http://json-schema.org/draft-04/schema#'},
+      {id: 'http://example.com/bar.schema.json', schema: 'http://json-schema.org/draft-04/schema#'},
+    ];
+
+    let shoulds = [
+      'schema/http://example.com/foo.schema.json',
+      'schema/http://example.com/bar.schema.json',
+    ];
+
+    let doc = await documenter({
+      schemas, // schema.id + content
     });
 
-    // testing tarball exists
-    assert.ok(doc.tarball);
+    let tarball = doc.tarball;
+
+    let extractor = tar.extract();
+    extractor.on('entry', (header, stream, callback) => {
+      let entryName = header.name;
+      let contains = false;
+
+      for (let expectedValue of shoulds) {
+        if (expectedValue == entryName) {
+          contains = true;
+          break;
+        }
+      }
+      assert.ok(contains);
+
+      stream.on('end', () => {
+        callback(); // ready for next entry
+      });
+
+      stream.resume(); // just auto drain the stream
+    });
+
+    extractor.on('finish', function() {
+      done();
+    });
+
+    tarball.pipe(extractor);
   });
 
   test('simplest case with nothing to do', async function() {
