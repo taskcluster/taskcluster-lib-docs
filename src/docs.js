639557f9fbdc2b210100c2873c8297d57ebd4145
Kristel Teng <kristelteng@gmail.com>
2016-07-08 14:12:39 -0400

    add docs to tarball

--- src/docs.js
+++ src/docs.js
@@ -1,13 +1,16 @@
 let tar = require('tar-stream');
 let _ = require('lodash');
 let assert = require('assert');
-let fs = require('fs');
+let fs = require('mz/fs');
 let path = require('path');
+let walk = require('walk');
+let recursiveReadSync = require('recursive-readdir-sync');
 
 async function documenter(options) {
 
   options = _.defaults({}, options, {
     schemas: [],
+    docsFolder: null,
   });
   assert(options.schemas, 'options.schemas must be given');
   assert(options.schemas instanceof Array, 'options.schemas must be an array');
@@ -15,11 +18,31 @@ async function documenter(options) {
   let schemas = options.schemas;
   let tarball = tar.pack();
 
-  //add schemas to tarball
+  // add schemas to tarball
   schemas.forEach(schema => {
     let data = JSON.stringify(schema, null, 2);
     tarball.entry({name: 'schema/' + schema.id}, data);
   });
+
+  // add docs to tarball
+  let docs = options.docsFolder;
+  let files = recursiveReadSync(options.docsFolder);
+
+  await Promise.all(files.map(file => {
+    //remove the path
+    let relativePath = file.replace(`${options.docsFolder}/`, '');
+    let fileReadPromise = new Promise(function(fulfill, reject) {
+      fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          tarball.entry({name: 'docs/' + relativePath}, data);
+          fulfill();
+        }
+      });
+    });
+    return fileReadPromise;
+  }));
 
   tarball.finalize();
   // pipe the pack stream somewhere
@@ -28,7 +51,6 @@ async function documenter(options) {
   let output = {
     tarball,
   };
-
   return output;
 }
 
