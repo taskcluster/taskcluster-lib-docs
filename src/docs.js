d310fe40f1e1460ea766f8f629dbe06ed92186d3
Kristel Teng <kristelteng@gmail.com>
2016-07-13 11:02:38 -0400

    Handle promises better for docs in tarball

--- src/docs.js
+++ src/docs.js
@@ -23,27 +23,21 @@ async function documenter(options) {
     tarball.entry({name: 'schema/' + schema.id}, data);
   });
 
-  // add docs to tarball
-  let docs = options.docsFolder;
-  let files = recursiveReadSync(options.docsFolder);
+  if (options.docsFolder !== null) {
+    // add docs to tarball
+    let docs = options.docsFolder;
+    let files = recursiveReadSync(options.docsFolder);
 
-  await Promise.all(files.map(file => {
-    //remove the path
-    let relativePath = file.replace(`${options.docsFolder}/`, '');
-    let fileReadPromise = new Promise(function(fulfill, reject) {
-      fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          tarball.entry({name: 'docs/' + relativePath}, data);
-          fulfill();
-        }
-      });
-    });
-    return fileReadPromise;
-  }));
+    await Promise.all(files.map(async (file) => {
+      //remove the path
+      let relativePath = path.basename(file);
+      let data = await fs.readFile(file, {encoding: 'utf8'});
+      tarball.entry({name: 'docs/' + relativePath}, data);
+    }));
+  }
 
   tarball.finalize();
+  // tarball.pipe(process.stdout);
 
   let output = {
     tarball,
