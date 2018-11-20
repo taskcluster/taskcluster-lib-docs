b234e961ffe3066b626ac33d66e7b4ae3472fd36
Dustin J. Mitchell <dustin@mozilla.com>
2018-11-20 19:41:58 +0000

    Bug 1508395 - update tc-lib-validate (devDep)

--- test/main_test.js
+++ test/main_test.js
@@ -199,7 +199,7 @@ suite('documenter', () => {
   test('writen schemas are abstract', async function() {
     await withWrittenDocs(docsDir => {
       const schema = JSON.parse(fs.readFileSync(path.join(docsDir, 'schemas', 'bar.json')));
-      assert.equal(schema.$id, 'taskcluster:/schemas/whatever/bar.json#');
+      assert.equal(schema.$id, '/schemas/whatever/bar.json#');
     });
   });
 
